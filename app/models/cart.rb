5b2fdaa1cb9611c2f85dfc58b0901a93f9b5a2bf
Kyle46220 <kyle@smithandthomas.com.au>
2019-11-08 09:54:56 +1100

    added order controllers model and views.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ApplicationRecord
   # belongs_to :profile
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
   has_many :cab_configs, through: :cart_items
   has_one :review, dependent: :destroy
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,5 @@
 class CartItem < ApplicationRecord
   belongs_to :cab_config
   belongs_to :cart
+  belongs_to :order
 end
